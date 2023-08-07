403ec5d48fc9be1de4c9934497234580dc8c0495
Chris <hakizachrist1@gmail.com>
2023-08-07 22:45:42 +0200

    Update home.tsx

---
+++ src/components/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../CSS/home.css';
+
+export const Home = () => {
+
+
+return (
+
+
+<div className="home-container">
+    <h1> I am home here </h1> 
+</div>
+
+)
+
+
+}
