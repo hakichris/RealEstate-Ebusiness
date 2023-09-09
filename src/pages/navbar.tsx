290b2e84c11031bca1c4cf7873d27fb691cb6a07
Chris <hakizachrist1@gmail.com>
2023-09-09 13:08:36 +0200

    Update navbars.tsx

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -1,40 +1,40 @@
-
+import logo from '../components/assets/hakikris-estate-logo.png'
 
 const Navbar = () => (
 
-  <div>
+  <div className="navigation">
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand fontsize" href="#"><img src={logo} alt="logo" className="logo" width="40" height="30" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active fontsize" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <a className="nav-link fontsize" href="#">About</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+          <a className="nav-link dropdown-toggle fontsize" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            More
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Land</a></li>
-            <li><a className="dropdown-item" href="#">Location</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Prices</a></li>
+            <li><a className="dropdown-item fontsize" href="#">Land</a></li>
+            <li><a className="dropdown-item fontsize" href="#">Location</a></li>
+            <li><hr className="dropdown-divider fontsize" /></li>
+            <li><a className="dropdown-item fontsize" href="#">Prices</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">News</a>
+          <a className="nav-link disabled fontsize" aria-disabled="true">News</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-success fontsize" type="submit">Search</button>
       </form>
     </div>
   </div>
